23826f6b9618e9eef747931054a3f1c64759ae74
Jahnavi Nellepalli Parthasarathi <npjahnavi99@gmail.com>
2022-10-31 02:26:47 -0400

    Update server.js
    Trivia API

--- src/server.js
+++ src/server.js
@@ -100,7 +100,6 @@ function shuffle(array) {
 
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
 
     // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
